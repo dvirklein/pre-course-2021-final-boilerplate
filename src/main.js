2c574d5ffdae8eb85001b32ac43a044c006f5664
dvirklein <77276010+dvirklein@users.noreply.github.com>
2021-01-30 16:36:42 +0200

    add the sort button`
    after a long time i have add function working button now im going to belo local!!!

--- src/main.js
+++ src/main.js
@@ -1,32 +1,69 @@
 addButton = document.getElementById("add-button");
-
-console.log(addButton);
 let viewSection = document.getElementById("view-section");
 let Body = document.body;
 Body.append(viewSection);
-
+// array of lists
+arrayOfLists = [];
+//
+let Thecounter = 0;
+let counterElement = document.getElementById("counter"); // defining counter
+counterElement.innerText = "There are " + Thecounter + " Todos";
+///
 addButton.addEventListener("click", (event) => {
-  let allInfo = document.createElement("div");
-  allInfo.classList.add("todo-container");
-  viewSection.append(allInfo);
+  if (document.getElementById("text-input").value !== null) {
+    createNewContainer();
+    resetingText();
+    //counter
+    Thecounter++;
+    counterElement.innerText = "There are " + Thecounter + " Todos";
+  }
+});
+
+sortButton = document.getElementById("sort-button");
+sortButton.addEventListener("click", sort);
+
+const createNewContainer = () => {
+  let Container = document.createElement("div");
+  Container.classList.add("todo-container");
+  viewSection.append(Container);
   //creating elements that will be:span-div-viewsection-body
   todoText = document.createElement("span");
   todoText.innerText = document.getElementById("text-input").value;
   todoText.classList.add("todo-text");
-  allInfo.append(todoText);
-
+  Container.append(todoText);
+  //
   let creationTime = document.createElement("span");
   let Dates = new Date();
   Dates = Dates.toISOString().slice(0, 19).replace("T", " "); ///turning date to sql by simple method
   creationTime.classList.add("todo-created-at");
   creationTime.innerText = Dates;
-  allInfo.append(creationTime);
-
+  Container.append(creationTime);
+  //
   let priorityNum = document.createElement("span");
   priorityNum.classList.add("todo-priority");
   priorityNum.innerText = document.getElementById("priority-selector").value;
-  allInfo.append(priorityNum);
+  Container.append(priorityNum);
 
-  document.getElementById("text-input").value = ""; //reseting every click
-  
-});
+  arrayOfLists.push(Container);
+};
+//
+const resetingText = () => {
+  document.getElementById("text-input").value = "";
+};
+//// sorting
+function sort() {
+  let sortArr = [];
+  for (let i = 5; i > 0; i--) {
+    for (let j = 0; j < arrayOfLists.length; j++) {
+      if (
+        arrayOfLists[j].querySelector(".todo-priority").innerText === String(i)
+      ) {
+        sortArr.push(arrayOfLists[j]);
+      }
+    }
+  }
+  // arrayOfLists = sortArr;
+  for (let item of sortArr) {
+    viewSection.append(item);
+  }
+}
